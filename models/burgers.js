08543998f1df4354f7a733e59430e23b093684a4
Jonathan Truong <jjtruong90@gmail.com>
2020-01-27 10:30:57 -0800

    Adds api-routes and Updates models

--- models/burgers.js
+++ models/burgers.js
@@ -21,3 +21,20 @@ var burger = {
 
 // Export the database functions for the controller.
 module.exports = burger;
+
+// module.exports = function(sequelize, DataTypes){
+//   var Burger = sequelize.define("Burger", {
+//     burger_name: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//       validate: {
+//         len: [1]
+//       }
+//     },
+//     devoured: {
+//       type: DataTypes.BOOLEAN,
+//       allowNull: false,
+//     }
+//   });
+      // return Burger;
+// }
